2759da06ead4b8b171de73561947a13bb2195808
Jorge Mateus <mateusnascimento07@hotmail.com>
2022-04-22 16:12:59 -0300

    tratando erro com excessoes personalizadas

--- Xadrez-Console/Tabuleiro/Tabuleiro.cs
+++ Xadrez-Console/Tabuleiro/Tabuleiro.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Xadrez_Console.xadrez;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,13 +22,44 @@ namespace Xadrez_Console.tabuleiro
 
         public Peca peca(int linha, int coluna)
         {
-            return Pecas[linha, coluna]; 
+            return Pecas[linha, coluna];
+        }
+
+        public Peca peca(Posicao pos)
+        {
+            return Pecas[pos.Linha, pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
         }
 
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if ( existePeca(pos) )
+            {
+                throw new TabuleiroException("Já existe uma peça nesta posição");
+            }
             Pecas[pos.Linha, pos.Coluna] = p;
             p.Posicao = pos;
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if ( pos.Linha < 0 || pos.Linha >= Linhas || pos.Coluna < 0 || pos.Coluna >= Colunas )
+            {
+                return false;
+            }
+                return true;
+        }
+        public void validarPosicao(Posicao pos)
+        {
+            if ( !posicaoValida(pos) )
+            {
+                throw new TabuleiroException("Posição inváida!");
+            }
         }
     }
 }
